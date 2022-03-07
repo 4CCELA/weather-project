d1a9dd40222bb2d350f7858088f1f9a6b30faa94
4CCELA <95509640+4CCELA@users.noreply.github.com>
2022-03-07 12:17:23 -0800

    Added API to weather icon

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ function showWeather(response) {
   );
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(event) {
