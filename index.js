0ed265441cde12d0396c6d749af4068491a5298c
4CCELA <95509640+4CCELA@users.noreply.github.com>
2022-03-08 15:18:21 -0800

    Replace svg with png

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function showWeather(response) {
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.svg`);
+  iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
 }
 
 function searchCity(event) {
